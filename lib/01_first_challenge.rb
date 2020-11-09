6e8d8a5745106e0ecf3de813172c16249050b511
Elias Campos <ecampose76@gmail.com>
2020-11-09 20:55:42 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -22,7 +22,34 @@ def first_challenge
          :name => "Mercutio",
          :age => "18",
          :attitude => "hot-headed"
-       }]
-     }
+       }
+       ]
+     },
+     :capulet => {
+       :patriarch => {
+         :name => "Lord Capulet",
+         :age => "50",
+       },
+       :matriarch => {
+         :name => "Lady Capulet",
+         :age => "51"
+       },
+       :heroine => {
+         :name => "Juliet",
+         :age => "15",
+         :status => "alive"
+       },
+       ::heroine_friends => [{
+         :name => "Steven",
+         :age => "30",
+         :attitude => "confused"
+       },
+       {
+         :name => "Nurse",
+         :age "44",
+         :attitude => "worried"
+       }
+     ]
    }
+ }
 end
